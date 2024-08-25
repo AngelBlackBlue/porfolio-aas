83b3d60cf611d100e3779d6ea22247b0f7c81b62
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-08-25 08:13:23 -0300

    refactorizacion

---
+++ src/data/tags.js
@@ -0,0 +1,125 @@
+
+import HtmlIcon from "@/icons/Html.astro"
+import CssIcon from "@/icons/Css.astro"
+import TailwindIcon from "@/icons/Tailwind.astro"
+import FlowbiteIcon from "@/icons/Flowbite.astro"
+import JavascriptIcon from "@/icons/Javascript.astro"
+import ReactIcon from "@/icons/React.astro"
+import ReduxToolkitIcon from "@/icons/ReduxToolkit.astro"
+import GoogleIcon from "@/icons/Google.astro"
+import TypescriptIcon from "@/icons/Typescript.astro"
+import NestJsIcon from "@/icons/Nestjs.astro"
+import CloudinaryIcon from "@/icons/Cloudinary.astro"
+import ViteIcon from "@/icons/Vite.astro"
+import JwtIcon from "@/icons/Jwt.astro"
+import SequelizeIcon from "@/icons/Sequelize.astro"
+import PostgreSQLIcon from "@/icons/PostgreSQL.astro"
+import SwaggerIcon from "@/icons/Swagger.astro"
+import NodeJsIcon from "@/icons/NodeJs.astro"
+import ExpressIcon from "@/icons/Express.astro"
+import NodeMailerIcon from "@/icons/NodeMailer.astro"
+import ZustandIcon from "@/icons/Zustand.astro"
+import ZodIcon from "@/icons/Zod.astro"
+import AstroIcon from "@/icons/AstroIcon.astro"
+import BunIcon from "@/icons/Bun.astro"
+
+export const TAGS = {
+    HTML: {
+        name: 'HTML',
+        icon: HtmlIcon,
+    },
+    CSS: {
+        name: 'CSS',
+        icon: CssIcon,
+    },
+    TAILWIND: {
+        name: 'Tailwind',
+        icon: TailwindIcon,
+    },
+    FLOWBITE: {
+        name: 'Flowbite',
+        icon: FlowbiteIcon,
+    },
+    JS: {
+        name: 'Javascript',
+        icon: JavascriptIcon,
+    },
+    VITE: {
+        name: 'Vite',
+        icon: ViteIcon,
+    },
+    
+    REACT: {
+        name: 'React',
+        icon: ReactIcon,
+    },
+    REDUXTOOLKIT: {
+        name: 'Redux Toolkit',
+        icon: ReduxToolkitIcon,
+    },
+    GOOGLEAOUTH: {
+           name: 'OAuth Google',
+           icon: GoogleIcon,
+    },
+    TYPESCRIPT: {
+        name: 'Typescript',
+        icon: TypescriptIcon,
+    },
+    NESTJS: {
+        name: 'NestJS',
+        icon: NestJsIcon,
+    },
+    CLOUDINARY: {
+        name: 'Cloudinary',
+        icon: CloudinaryIcon,
+    },
+    JWT: {
+        name: 'JWT',
+        icon: JwtIcon,
+    },
+    SEQUELIZE: {
+        name: 'Sequelize',
+        icon: SequelizeIcon,
+    },
+    POSTGRESQL: {
+        name: 'PostgreSQL',
+        icon: PostgreSQLIcon,
+    },
+    SWAGGER: {
+        name: 'Swagger',
+        icon: SwaggerIcon,
+    },
+    NODEJS: {
+        name: 'NodeJS',
+        icon: NodeJsIcon,
+    },
+    EXPRESS: {
+        name: 'Express',
+        icon: ExpressIcon,
+    },
+    NODEMAILER: {
+        name: 'NodeMailer',
+        icon: NodeMailerIcon,
+    },
+    ZUSTAND: {
+        name: 'Zustand',
+        icon: ZustandIcon,      
+    },
+    ZOD: {
+        name: 'Zod',
+        icon: ZodIcon,
+    },
+    REDUX: {
+        name: 'Redux',
+        icon: ReduxToolkitIcon,
+    },  
+    ASTRO: {
+        name: 'Astro',
+        icon: AstroIcon,
+    },
+    BUN: {
+        name: 'Bun',
+        icon: BunIcon,
+    },        
+        
+}
